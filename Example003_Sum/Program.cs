da4f52601185154c5a0f6f78174c3f5b3bd89bf8
АЛЕКСАНДР ЛОЗИЦКИЙ <avtolik2015@outlook.com>
2022-11-19 17:44:18 +0300

    Example003_SumRandom

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -2,7 +2,14 @@
 int numberB = 5;
 Console.WriteLine(numberA + numberB);*/
 
-int numberA = 36;
+/*int numberA = 36;
 int numberB = 3;
+int result = numberA + numberB;
+Console.WriteLine(result);*/
+
+int numberA = new Random().Next(1, 10); // 1234 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
